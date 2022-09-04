8de475d47e1cf0139c5a5e255fd5e6b8db021de4
Rachel Okorie <jayrachel96@gmail.com>
2022-09-04 10:04:04 +0100

    Created main file

---
+++ CSharpTasks.cs
@@ -0,0 +1,12 @@
+using GuessTheNumber;
+using LeapYearAnnouncer;
+using PigLatin;
+
+class CSharpTasks {
+    public static void Main() {
+      // GuessTheNumber.PlayGuess();
+      // LeapYearAnnouncer.PrintLeapYears();
+      // PigLatin.TranslateToPigLatin();
+}
+
+}
